479aa9528743e81f8526d936ecf024f0e50c9af7
robotdostopif <aron.cederlund@outlook.com>
2023-02-12 21:33:16 +0100

    Update config with mysql database

--- procrastinate/settings/production/profile.py
+++ procrastinate/settings/production/profile.py
@@ -5,5 +5,16 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 ALLOWED_HOSTS = [ 'kamelijoint.pythonanywhere.com', '127.0.0.1' ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST':os.environ.get('MYSQL_HOST')
+        'PORT':os.environ.get('MYSQL_PORT'),
+    }
+}
+
 DEBUG = False
 
