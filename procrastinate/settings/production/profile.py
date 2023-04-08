134bd7956c703fc21114c6e6182bbc165bd03d41
robotdostopif <aron.cederlund@outlook.com>
2023-04-08 22:25:40 +0200

    Get allowed hosts from environment variable

--- procrastinate/settings/production/profile.py
+++ procrastinate/settings/production/profile.py
@@ -2,7 +2,7 @@ import os
 from procrastinate.settings.base import *
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-ALLOWED_HOSTS = [ 'kamelijoint.pythonanywhere.com', '127.0.0.1' ]
+ALLOWED_HOSTS = os.environ.get('PROCRASTINATE_ALLOWED_HOSTS').split(',')
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 DATABASES = {
