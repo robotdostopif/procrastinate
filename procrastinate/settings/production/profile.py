b39fc2896843c49f9e3076735305dad6f5563bef
robotdostopif <aron.cederlund@outlook.com>
2023-04-09 09:01:06 +0200

    Fix missing comma in databases settings

--- procrastinate/settings/production/profile.py
+++ procrastinate/settings/production/profile.py
@@ -11,7 +11,7 @@ DATABASES = {
         'NAME': os.environ.get('MYSQL_DATABASE'),
         'USER': os.environ.get('MYSQL_USER'),
         'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
-        'HOST':os.environ.get('MYSQL_HOST')
+        'HOST':os.environ.get('MYSQL_HOST'),
         'PORT':os.environ.get('MYSQL_PORT'),
     }
 }
